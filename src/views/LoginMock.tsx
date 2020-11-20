3de3618390234526146f9339602448d490692b40
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-20 22:55:14 +0200

    Add loading to LoginMock.tsx

--- src/views/LoginMock.tsx
+++ src/views/LoginMock.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from 'react';
 
+import { Button } from 'antd';
+
 import { UserContext } from 'contexts';
 
 const LoginMock: React.FC = () => {
-  const { login } = useContext(UserContext);
+  const { login, loggingIn } = useContext(UserContext);
 
   const [email, setEmail] = useState('andrei@mail.com');
   const [password, setPassword] = useState('123456');
@@ -14,7 +16,9 @@ const LoginMock: React.FC = () => {
       <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
       password
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={() => login(email, password)}>login</button>
+      <Button loading={loggingIn} onClick={() => login(email, password)}>
+        login
+      </Button>
     </div>
   );
 };
