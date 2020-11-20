f7811aef12d75d04f313cb09ab2900854e469162
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-20 22:50:32 +0200

    Enforce user login at Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import { UserContext } from 'contexts';
 
 import LoginMock from './views/LoginMock';
 
 const Routes: React.FC = () => {
+  const { user } = useContext(UserContext);
+
+  const LoginEnforcer = (
+    <>
+      <Route path="/login">
+        <LoginMock />
+      </Route>
+      <Redirect to="/login" />
+    </>
+  );
+
+  const Content = (
+    <>
+      <Route exact path="/">
+        <h1>Hello unihack</h1>
+      </Route>
+      <Redirect to="/" />
+    </>
+  );
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          <LoginMock />
-        </Route>
-        <Route exact path="/">
-          <h1>Hello unihack</h1>
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <Switch>{user ? Content : LoginEnforcer}</Switch>
     </BrowserRouter>
   );
 };
