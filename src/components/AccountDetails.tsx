03751f134aed65864982c604537a93eb0aee409e
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-22 01:18:09 +0200

    Display real user name in header

--- src/components/AccountDetails.tsx
+++ src/components/AccountDetails.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { UserContext } from 'contexts';
 
 const AccountDetails: React.FC = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="accountDetails">
-      <div className="accountDetails__username">username</div>
+      <div className="accountDetails__username">{user.displayName}</div>
       <div className="accountDetails__icon"></div>
       <div className="accountDetails__dropdown"></div>
     </div>
