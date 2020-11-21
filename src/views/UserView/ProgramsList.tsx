94eab2b5eda709dc3c52c2423c14e68c4b11f68d
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-21 03:18:18 +0200

    Create programList

---
+++ src/views/UserView/ProgramsList.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+
+import { List } from 'antd';
+import { BarChartOutlined, ClockCircleOutlined } from '@ant-design/icons';
+
+import { Program, StudentContext } from 'contexts';
+
+const CourseList: React.FC = () => {
+  const { allPrograms } = useContext(StudentContext);
+
+  const DIFFICULTIES = ['Easy', 'Medium', 'Hard'];
+
+  const makeListItem = (item: Program) => {
+    return (
+      <div className="listItem">
+        <img src={item.thumbnailUrl} alt="" className="listItem__img" />
+        <div className="listItem__content">
+          <div className="listItem__tags">{item.tags.join(' - ')}</div>
+          <div className="listItem__title">{item.title}</div>
+          <div className="listItem__stats">
+            <div className="listItem__stat">
+              <BarChartOutlined />
+              {DIFFICULTIES[item.difficulty]}
+            </div>
+            <div className="listItem__stat">
+              <ClockCircleOutlined />
+              {item.duration}
+            </div>
+          </div>
+          <div className="listItem__description">{item.description}</div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="CoursesList">
+      <div className="main wrapper">
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={allPrograms}
+          renderItem={(item) => <List.Item> {makeListItem(item)} </List.Item>}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CourseList;
