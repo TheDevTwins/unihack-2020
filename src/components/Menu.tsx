c09d64ba235b0014ac60d91a11f03e2cf341c9d2
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-21 03:02:31 +0200

    Add links to header

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu: React.FC = () => {
   return (
     <div className="menu">
-      <div className="menu__item active">Programs</div>
-      <div className="menu__item">Courses</div>
-      <div className="menu__item">Discover New</div>
+      <div className="menu__item active">
+        <Link to={`/programs`}>Program</Link>
+      </div>
+      <div>
+        <Link className="menu__item" to={`/courses`}>
+          Courses
+        </Link>
+      </div>
+      <div className="menu__item">
+        <Link to={`/discover`}>Discover New</Link>
+      </div>
     </div>
   );
 };
