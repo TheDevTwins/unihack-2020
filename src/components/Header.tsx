fe87e87322809567e9ba047a87eb1262adb68dc4
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-21 05:07:23 +0200

    Move header into components folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+
+import { Logo, Menu, AccountDetails } from 'components';
+
+import { UserContext } from 'contexts';
+
+type Props = {
+  buttons: { text: string; url: string }[];
+};
+
+const Header: React.FC<Props> = ({ buttons }) => {
+  const { logout } = useContext(UserContext);
+
+  return (
+    <div className="header">
+      <div className="wrapper">
+        <div className="header__container">
+          <Logo />
+          <Menu buttons={buttons} />
+          <AccountDetails />
+          <button onClick={logout}>logout</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/views/UserView/index.tsx
+++ src/views/UserView/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Header from './Header';
+import { Header } from 'components';
 import CourseList from './CoursesList';
 import ProgramsList from './ProgramsList';
 
 const UserView: React.FC = () => {
   return (
     <div>
-      <Header />
+      <Header
+        buttons={[
+          {
+            text: 'Programs',
+            url: '/programs',
+          },
+          {
+            text: 'Courses',
+            url: '/courses',
+          },
+          {
+            text: 'Discover new',
+            url: '/discover',
+          },
+        ]}
+      />
       <Switch>
         <Route path={`/programs`}>
           <ProgramsList />
