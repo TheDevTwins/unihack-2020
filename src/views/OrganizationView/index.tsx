db57515fd86d09bfea116a60251525978e221a48
Kriss Gardner <krissrobert12@gmail.com>
2020-11-21 16:22:47 +0200

    Create program list and routes for org view

--- src/views/OrganizationView/index.tsx
+++ src/views/OrganizationView/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { Header } from 'components';
+import ProgramList from './ProgramList';
+import CreateProgram from './CreateProgram';
 
 const OrganizationView: React.FC = () => {
   return (
     <div className="OrgView">
       <Header />
 
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <Switch>
+          <Route path={`/programs`}>
+            <ProgramList />
+          </Route>
+          <Route path={`/create`}>
+            <CreateProgram />
+          </Route>
+          <Redirect to={`/programs`} />
+        </Switch>
+      </div>
     </div>
   );
 };
