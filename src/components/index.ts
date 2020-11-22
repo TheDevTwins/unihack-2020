9e267858673fcdd27f2126106c626e6e65c3b1ce
Andrei Stoica <andrei00stoica@gmail.com>
2020-11-22 06:01:13 +0200

    Create LoadingWrapper.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Logo } from './Logo';
 export { default as Menu } from './Menu';
 export { default as AccountDetails } from './AccountDetails';
 export { default as Header } from './Header';
+export { default as LoadingWrapper } from './LoadingWrapper';
